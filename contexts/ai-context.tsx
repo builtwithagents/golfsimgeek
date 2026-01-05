d639d2d51efb5898690a618694cb492f4ffc22f5
Piotr Kulpinski <piotr@kulpinski.pl>
2026-01-05 22:23:34 +0100

    fix: allow using useAI hook outside of the provider

--- contexts/ai-context.tsx
+++ contexts/ai-context.tsx
@@ -20,7 +20,7 @@ export const useAI = () => {
   const context = use(AIContext)
 
   if (!context) {
-    throw new Error("useAI must be used within an AIProvider")
+    return { isAIEnabled: false }
   }
 
   return context
