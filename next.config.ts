6211b12ec48087ebe2f61748164be67c49d7543e
Piotr Kulpinski <piotr@kulpinski.pl>
2026-03-18 22:45:41 +0100

    chore: upgrade to next.js 16.2 and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,7 @@ const nextConfig: NextConfig = {
     useCache: true,
     turbopackFileSystemCacheForDev: true,
     turbopackFileSystemCacheForBuild: true,
+    prefetchInlining: true,
   },
 
   images: {
